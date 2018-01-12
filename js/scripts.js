4ff5653e7e1bbcef78c22f73d893cd4a638a76d1
Cameron Anderson <chamburg321@gmail.com>
2018-01-12 11:37:48 -0800

    show one question at a time

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,33 @@
-//ruby, php, java, css, c#
-var scores = [0,0,0,0,0];
-var question = 1;
-
 $(document).ready(function() {
 	
-	
+	//ruby, php, java, css, c#
+	var scores = [0,0,0,0,0];
 
 	function addScore(index) {
 		scores[index] += 1;
 	}
 	
-	
+	var question = 0;
 	$("#form1").submit(function(event) {
 		event.preventDefault();
-	
-		addScore(parseInt($("input:radio[name=q" + question + "]:checked").val()));
+		
 		question++;
 		
-		console.log(scores);
-		
-		if (question > 5) {
-			question = 1;
+
+		if (question === $(".question").length + 1) {
+			alert("results");
+		} else if (question === $(".question").length + 2) {
+			question = 0;
 			scores = [0,0,0,0,0];
+			alert("reseting");
+		} else {
+			addScore(parseInt($("input:radio[name=q" + question + "]:checked").val()));
 		}
+		
+		
+		$(".row").hide();
+		$("#q" + question + ".row").fadeIn();
+		console.log(scores);
 	});
 	
 });
