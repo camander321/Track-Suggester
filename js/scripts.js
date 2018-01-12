2d208353ce3867a618e4f5530c2cf0b8211297df
Cameron Anderson <chamburg321@gmail.com>
2018-01-12 11:05:13 -0800

    gather form input for scoring

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,28 @@
+//ruby, php, java, css, c#
+var scores = [0,0,0,0,0];
+var question = 1;
+
 $(document).ready(function() {
+	
+	
+
+	function addScore(index) {
+		scores[index] += 1;
+	}
+	
 	
 	$("#form1").submit(function(event) {
 		event.preventDefault();
+	
+		addScore(parseInt($("input:radio[name=q" + question + "]:checked").val()));
+		question++;
 		
+		console.log(scores);
 		
-		alert($("input:radio[name=q1]:checked").val());
-		alert($("input:radio[name=q2]:checked").val());
+		if (question > 5) {
+			question = 1;
+			scores = [0,0,0,0,0];
+		}
 	});
 	
 });
