769c00725d517f2995fe2597f574a6c28dad4fc2
Cameron Anderson <chamburg321@gmail.com>
2018-01-12 12:18:35 -0800

    fix bug with last question not being scored

--- js/scripts.js
+++ js/scripts.js
@@ -2,32 +2,46 @@ $(document).ready(function() {
 	
 	//ruby, php, java, css, c#
 	var scores = [0,0,0,0,0];
+	var question = 0;
 
 	function addScore(index) {
 		scores[index] += 1;
 	}
 	
-	var question = 0;
+	function nextQuestion() {
+		$(".row").hide();
+		$("#q" + (question + 1) + ".row").fadeIn();
+	}
+	
 	$("#form1").submit(function(event) {
 		event.preventDefault();
 		
-		question++;
-		
-
-		if (question === $(".question").length + 1) {
-			alert("results");
-		} else if (question === $(".question").length + 2) {
-			question = 0;
-			scores = [0,0,0,0,0];
-			alert("reseting");
-		} else {
+		if (question === 0) {
+			nextQuestion();
+			$("button").text("Next Question");
+		} 
+		else if (question === $(".question").length) {
 			addScore(parseInt($("input:radio[name=q" + question + "]:checked").val()));
+			console.log(scores + " | Q#" + question);
+			alert("results");
+			$(".row").hide();
+			$("button").text("Restart");
+		} 
+		else if (question === $(".question").length + 1) {
+			question = -1;
+			scores = [0,0,0,0,0];
+			$("button").text("Begin Quiz!");
+		} 
+		else {
+			nextQuestion();
+			if (question === $(".question").length - 1)
+				$("button").text("Get Results!");
+			
+			addScore(parseInt($("input:radio[name=q" + question + "]:checked").val()));
+			console.log(scores + " | Q#" + question);
 		}
 		
-		
-		$(".row").hide();
-		$("#q" + question + ".row").fadeIn();
-		console.log(scores);
+		question++;
 	});
 	
 });
