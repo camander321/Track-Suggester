fd3587152388f703137f65284fd8ab263eb3bec2
Cameron Anderson <chamburg321@gmail.com>
2018-01-12 09:41:14 -0800

    format basic question template

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,11 @@
 $(document).ready(function() {
+	
+	$("#form1").submit(function(event) {
+		event.preventDefault();
+		
+		
+		alert($("input:radio[name=q1]:checked").val());
+		alert($("input:radio[name=q2]:checked").val());
+	});
+	
 });
